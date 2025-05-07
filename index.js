40e82f0f04cb4ab1229e983602dfc1ffce3c2089
KGushti <kgushti@gmail.com>
2025-05-07 10:47:11 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>مرحبا بك في متجر واقد الإلكتروني</h1>
+      <p>قريبًا سيتم عرض المنتجات والعروض هنا</p>
+    </div>
+  );
+}
